8b55ef77c60bfd5279c06547319c78cf43215222
Nick <silent.github@transparentage.com>
2012-07-03 23:39:33 +0300

    FeedWriter: Numerous changes from branch:rss-dev  - Stylistic updates  - Uses new Bucket Public Tokens

--- classes/controller/feedwriter.php
+++ classes/controller/feedwriter.php
@@ -12,103 +12,115 @@
 include 'plugins/feedwriter/classes/libs/rss.php';
 include 'plugins/feedwriter/classes/libs/atom.php';
 
-class Controller_Feedwriter extends Controller_Swiftriver
-{
-    protected $bucket;
-    protected $droplets;
-    protected $ready;
+class Controller_Feedwriter extends Controller_Swiftriver {
+	protected $bucket;
+	protected $droplets;
+	protected $ready;
 
-    public function before()
-    {
-        parent::before();
-        $this->auto_render = false;
-        $this->bucket = ORM::factory('bucket')
-            ->where('bucket_name_url', '=', $this->request->param('name'))
-            ->find();
+	public function before()
+	{
+		parent::before();
+		$this->auto_render = FALSE;
+		$this->bucket = ORM::factory('bucket')
+		     ->where('bucket_name_url', '=', $this->request->param('name'))
+		     ->find();
 
-        // If load successful, grab most recent 10 droplets and save
-        if ($this->bucket->loaded())
-        {
-            $droplets = Model_Bucket::get_droplets($this->user->id,
-                $this->bucket->id, 0, NULL, PHP_INT_MAX, FALSE, array(), 10);
-            $this->droplets = $droplets['droplets'];
-        }
-        else
-            throw new HTTP_Exception_404();
-    }
+		if ($this->bucket->loaded())
+		{
+			// Bucket isn't published and logged in user isn't owner
+			if ( ! $this->bucket->bucket_publish AND
+			    ! $this->bucket->is_owner($this->user->id) AND
+			   ( ! $this->bucket->public_token OR
+			   Arr::get($_GET, 't', '') != $this->bucket->public_token))
+			{
+				throw new HTTP_Exception_404();
+			}
 
-    public function action_rss()
-    {
-        $account = $this->bucket->account->account_path;
-        $name = $this->bucket->bucket_name;
+			$droplets = Model_Bucket::get_droplets($this->user->id,
+			            $this->bucket->id, 0, NULL, PHP_INT_MAX, FALSE, array(),
+			            10);
+			$this->droplets = $droplets['droplets'];
+		}
+		else
+		{
+			throw new HTTP_Exception_404();
+		}
+	}
 
-        $feed = new Feedwriter_Rss;
-        $feed->set_title($account.' / '.$name);
-        $feed->set_link(URL::site($this->bucket->get_base_url(), true));
-        $feed->set_description('Drops from '.$this->bucket->bucket_name.
-            ' on Swiftriver');
-        $feed->set_language(Kohana::$config->load('feedwriter')->get('language'));
-        $feed->set_copyright(Kohana::$config->load('feedwriter')->get('copyright'));
+	public function action_rss()
+	{
+		$account = $this->bucket->account->account_path;
+		$name = $this->bucket->bucket_name;
 
-        if (count($this->droplets) > 0)
-        {
-            $feed->set_updated($this->droplets[0]['droplet_date_pub']);
-            foreach ($this->droplets as $k => $v)
-            {
-                $url = URL::site($this->request->param('account').'/bucket/'.
-                    $this->request->param('name').'/drop/'.$v['id'], true);
-                $feed->add_item(array(
-                    'title'       => $v['droplet_title'],
-                    'guid'        => $url,
-                    'link'        => $url,
-                    'description' => $v['droplet_content'],
-                    'time'        => $v['droplet_date_pub']
-                ));
-            }
-        }
+		$feed = new Feedwriter_Rss;
+		$feed->set_title($account.' / '.$name);
+		$feed->set_link(URL::site($this->bucket->get_base_url(), TRUE));
+		$feed->set_description('Drops from '.$this->bucket->bucket_name
+		                      .' on Swiftriver');
+		$feed->set_language(Kohana::$config->load('feedwriter')->get('language'));
+		$feed->set_copyright(Kohana::$config->load('feedwriter')->get('copyright'));
 
-        $gen = $feed->generate();
-        $this->response->headers('Content-Type', 'text/xml');
-        $this->response->headers('ETag', md5($gen));
-        echo $gen;
-    }
-    
-    public function action_atom()
-    {
-        $account = $this->bucket->account->account_path;
-        $name = $this->bucket->bucket_name;
+		if (count($this->droplets) > 0)
+		{
+			$feed->set_updated($this->droplets[0]['droplet_date_pub']);
+			foreach ($this->droplets as $k => $v)
+			{
+				$url = URL::site($this->request->param('account').'/bucket/'
+				                .$this->request->param('name').'/drop/'
+				                .$v['id'], TRUE);
+				$feed->add_item(array(
+					'title'       => $v['droplet_title'],
+					'guid'        => $url,
+					'link'        => $url,
+					'description' => $v['droplet_content'],
+					'time'        => $v['droplet_date_pub']
+				));
+			}
+		}
 
-        $feed = new Feedwriter_Atom;
-        $feed->set_title($account.' / '.$name);
-        $feed->set_link(URL::site($this->bucket->get_base_url(), true));
-        $feed->set_author('SwiftRiver / '.$account, URL::site($account, true));
-        $feed->set_description('Drops from '.$this->bucket->bucket_name.
-            ' on Swiftriver');
-        $feed->set_copyright(Kohana::$config->load('feedwriter')->get('copyright'));
+		$gen = $feed->generate();
+		$this->response->headers('Content-Type', 'text/xml');
+		$this->response->headers('ETag', md5($gen));
+		echo $gen;
+	}
 
-        if (count($this->droplets) > 0)
-        {
-            $feed->set_updated($this->droplets[0]['droplet_date_pub']);
-            foreach ($this->droplets as $k => $v)
-            {
-                $url = URL::site($this->request->param('account').'/bucket/'.
-                    $this->request->param('name').'/drop/'.$v['id'], true);
-                $feed->add_item(array(
-                    'title'   => $v['droplet_title'],
-                    'id'      => $url,
-                    'link'    => $url,
-                    'content' => $v['droplet_content'],
-                    'time'    => $v['droplet_date_pub'],
-                    'author'  => $v['identity_name'],
-                ));
-            }
-        }
+	public function action_atom()
+	{
+		$account = $this->bucket->account->account_path;
+		$name = $this->bucket->bucket_name;
 
-        $gen = $feed->generate();
-        $this->response->headers('Content-Type', 'text/xml');
-        $this->response->headers('ETag', md5($gen));
-        echo $gen;
-    }
+		$feed = new Feedwriter_Atom;
+		$feed->set_title($account.' / '.$name);
+		$feed->set_link(URL::site($this->bucket->get_base_url(), TRUE));
+		$feed->set_author('SwiftRiver / '.$account, URL::site($account, TRUE));
+		$feed->set_description('Drops from '.$this->bucket->bucket_name.
+		                      .' on Swiftriver');
+		$feed->set_copyright(Kohana::$config->load('feedwriter')->get('copyright'));
+
+		if (count($this->droplets) > 0)
+		{
+			$feed->set_updated($this->droplets[0]['droplet_date_pub']);
+			foreach ($this->droplets as $k => $v)
+			{
+				$url = URL::site($this->request->param('account').'/bucket/'
+				                .$this->request->param('name').'/drop/'
+				                .$v['id'], TRUE);
+				$feed->add_item(array(
+					'title'   => $v['droplet_title'],
+					'id'      => $url,
+					'link'    => $url,
+					'content' => $v['droplet_content'],
+					'time'    => $v['droplet_date_pub'],
+					'author'  => $v['identity_name'],
+				));
+			}
+		}
+
+		$gen = $feed->generate();
+		$this->response->headers('Content-Type', 'text/xml');
+		$this->response->headers('ETag', md5($gen));
+		echo $gen;
+	}
 }
 
 ?>
--- init.php
+++ init.php
@@ -11,33 +11,41 @@
 
 class Feedwriter_Init
 {
-    public static function InjectMeta()
-    {
-        if (Request::current()->controller() == 'bucket' &&
-            strlen(Request::current()->param('name')) > 0)
-        {
-            $rss_url = URL::site(Route::get('feeds')->uri(array(
-                'account' => Request::current()->param('account'),
-                'name'    => Request::current()->param('name'),
-                'action'  => 'rss'
-            )), true);
-            $atom_url = URL::site(Route::get('feeds')->uri(array(
-                'account' => Request::current()->param('account'),
-                'name'    => Request::current()->param('name'),
-                'action'  => 'atom'
-            )), true);
-            echo '<link rel="alternate" title="RSS" type="application/rss+xml" href="'.$rss_url.
-                '" /><link rel="alternate" title="Atom" type="application/atom+xml" href="'.
-                $atom_url.'" />';
-        }
-    }
+	public static function InjectMeta()
+	{
+		if (Request::current()->controller() == 'bucket' &&
+			strlen(Request::current()->param('name')) > 0)
+		{
+			$rss_url = URL::site(Route::get('feeds')->uri(array(
+				'account' => Request::current()->param('account'),
+				'name'	  => Request::current()->param('name'),
+				'action'  => 'rss'
+			)), true);
+			$atom_url = URL::site(Route::get('feeds')->uri(array(
+				'account' => Request::current()->param('account'),
+				'name'	  => Request::current()->param('name'),
+				'action'  => 'atom'
+			)), true);
+			echo '<link rel="alternate" title="RSS" type="application/rss+xml" href="'.$rss_url.
+				'" /><link rel="alternate" title="Atom" type="application/atom+xml" href="'.
+				$atom_url.'" />';
+		}
+	}
 
-    public static function InjectIcon()
-    {
-        if (Request::current()->controller() == 'bucket' &&
-            strlen(Request::current()->param('name')) > 0)
-            echo HTML::script("plugins/feedwriter/media/js/icon.js");
-    }
+	public static function InjectIcon()
+	{
+		$visited_account = ORM::factory('account',
+			array('account_path' => Request::current()->param('account')));
+		
+		$bucket = ORM::factory('bucket')
+			->where('bucket_name_url', '=', Request::current()->param('name'))
+			->where('account_id', '=', $visited_account->id)
+			->find();
+
+		if (Request::current()->controller() == 'bucket' &&
+			strlen(Request::current()->param('name')) > 0)
+			echo HTML::script("plugins/feedwriter/media/js/icon.php?t=".$bucket->public_token);
+	}
 }
 
 Swiftriver_Event::add('swiftriver.template.meta', array('Feedwriter_Init', 'InjectMeta'));
@@ -45,13 +53,13 @@ Swiftriver_Event::add('swiftriver.template.head', array('Feedwriter_Init', 'Inje
 
 // Bind the plugin to valid URLs
 Route::set('feeds', '<account>/bucket/<name>/<action>',
-    array(
-        'action' => '(rss|atom)'
-    ))
-    ->defaults(array(
-        'controller' => 'feedwriter',
-        'action' => 'generate'
-    ));
+	array(
+		'action' => '(rss|atom)'
+	))
+	->defaults(array(
+		'controller' => 'feedwriter',
+		'action' => 'generate'
+	));
 
 
 
